1ddf7c24a77c164cd60d0c48f1bd22983fd21de0
Matthew Avarello <182387652+MatthewAvarello@users.noreply.github.com>
2026-05-03 23:24:57 -0400

    test stuff

---
+++ index.js
@@ -0,0 +1,15 @@
+import http from 'node:http'
+import url from 'node:url'
+import fs from 'node:fs'
+
+
+const server = http.createServer((request,response) => {
+    console.log("request recieved broski!")
+    //console.log(request.url)
+    response.writeHead(200,"Request Recieved!",{"Content-Type": 'text/plain'})
+    response.end("Hello and Goodbye!")
+})
+
+server.listen(8080,'localhost',() => {
+    console.log("server running")
+})
