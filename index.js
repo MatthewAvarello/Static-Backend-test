4206435373d3d05e993c33f2f29e0fdbf57f77a3
Matthew Avarello <182387652+MatthewAvarello@users.noreply.github.com>
2026-05-04 23:22:41 -0400

    Quick transfer 2 min

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ import http from 'node:http'
 import url from 'node:url'
 import fs from 'node:fs'
 
-
+const pages = {
+    "/": "eeeeeee"
+}
 const server = http.createServer((request,response) => {
     console.log("request recieved broski!")
-    //console.log(request.url)
+    let url = request.url
+    console.log(pages[url])
     response.writeHead(200,"Request Recieved!",{"Content-Type": 'text/plain'})
     response.end("Hello and Goodbye!")
 })
